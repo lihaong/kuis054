a17aebbf739e15d11dbb8bd114195f3db5474c16
Muhammad Fajar Andikha <andhikadika7878@gmail.com>
2023-03-25 15:27:11 +0700

    update bg

--- lib/pokemon_detail_page.dart
+++ lib/pokemon_detail_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kuispraktikum/pokemon_data.dart';
 import 'pokemon_data.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 
 class PokemonDetailPage extends StatefulWidget {
   final PokemonData pokemon;
@@ -13,145 +13,183 @@ class PokemonDetailPage extends StatefulWidget {
 }
 
 class _PokemonDetailPageState extends State<PokemonDetailPage> {
-
   void _toggleFavorite() {
     setState(() {
       widget.pokemon.isFavorite = !widget.pokemon.isFavorite;
     });
-    final message = widget.pokemon.isFavorite ? 'Berhasil Menambahkan ke Favorit' : 'Berhasil Menghapus dari Favorit';
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+    final message = widget.pokemon.isFavorite
+        ? 'Berhasil Menambahkan ke Favorit'
+        : 'Berhasil Menghapus dari Favorit';
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.pokemon.name),
-        actions: [
-          IconButton(
-            icon: Icon(
-              widget.pokemon.isFavorite ? Icons.favorite : Icons.favorite_border,
-              color: Colors.white,
+        appBar: AppBar(
+          title: Text(widget.pokemon.name),
+          actions: [
+            IconButton(
+              icon: Icon(
+                widget.pokemon.isFavorite
+                    ? Icons.favorite
+                    : Icons.favorite_border,
+                color: Colors.white,
+              ),
+              onPressed: _toggleFavorite,
             ),
-            onPressed: _toggleFavorite,
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-
-          // Add your onPressed code here!
-        },
-        backgroundColor: Colors.red,
-        child: const Icon(Icons.search),
-      ),
-      body: ListView(children: [
-        Column(
-          children: [
-            Center(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: Image.network(
-                      widget.pokemon.image,
-                      width: 300,
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            if (await canLaunch(widget.pokemon.wikiUrl)) {
+              await launch(widget.pokemon.wikiUrl);
+            } else {
+              throw 'Could not launch ${widget.pokemon.wikiUrl}';
+            }
+          },
+          child: const Icon(Icons.travel_explore),
+        ),
+        body: ListView(children: [
+          Column(
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Image.network(
+                        widget.pokemon.image,
+                        width: 300,
+                      ),
                     ),
-                  ),
-                  Text(
-                    widget.pokemon.name,
-                    style:
-                    TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-                  ),
-                  Center(
-                      child: Column(children: <Widget>[
-                        Container(
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(15)),
-                          ),
-                          margin: EdgeInsets.all(20),
-                          child: Table(
-                            columnWidths: {
-                              0: FlexColumnWidth(50),
-                              1: FlexColumnWidth(300),
-                            },
-                            children: [
-                              TableRow(children: [
-                                Column(
-                                  children: [Text("Type", style: TextStyle(fontWeight: FontWeight.bold))],
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                ),
-                              ]),
-                              TableRow(children: [
-                                Column(
-                                  children: [Text( "[ " + widget.pokemon.type.join(", ") + " ]")],
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                ),
-                              ]),
-                              TableRow(children: [
-                                Column(
-                                  children: [Text(" ")],
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                ),
-                              ]),
-                              TableRow(children: [
-                                Column(
-                                  children: [Text("Weakness", style: TextStyle(fontWeight: FontWeight.bold))],
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                ),
-                              ]),
-                              TableRow(children: [
-                                Column(
-                                  children: [Text("[ " +widget.pokemon.weakness.join(", ")+ " ]")],
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                ),
-                              ]),
-                              TableRow(children: [
-                                Column(
-                                  children: [Text(" ")],
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                ),
-                              ]),
+                    Text(
+                      widget.pokemon.name,
+                      style:
+                          TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                    ),
+                    Center(
+                        child: Column(children: <Widget>[
+                      Container(
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                        margin: EdgeInsets.all(20),
+                        child: Table(
+                          columnWidths: {
+                            0: FlexColumnWidth(50),
+                            1: FlexColumnWidth(300),
+                          },
+                          children: [
                             TableRow(children: [
                               Column(
-                                children: [Text("Previous Evolution", style: TextStyle(fontWeight: FontWeight.bold))],
+                                children: [
+                                  Text("Type",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold))
+                                ],
                                 crossAxisAlignment: CrossAxisAlignment.center,
                               ),
                             ]),
-                              TableRow(children: [
-                                Column(
-                                  children: [Text("[ " +widget.pokemon.prevEvolution.join(", ")+ " ]")],
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                ),
-                              ]),
-                              TableRow(children: [
-                                Column(
-                                  children: [Text(" ")],
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                ),
-                              ]),
-                              TableRow(children: [
-                                Column(
-                                  children: [Text("Next Evolution", style: TextStyle(fontWeight: FontWeight.bold))],
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                ),
-                              ]),
-                              TableRow(children: [
-                                Column(
-                                  children: [Text("[ " +widget.pokemon.nextEvolution.join(", ")+" ]")],
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                ),
-                              ]),
-                            ],
-                          ),
+                            TableRow(children: [
+                              Column(
+                                children: [
+                                  Text("[ " +
+                                      widget.pokemon.type.join(", ") +
+                                      " ]")
+                                ],
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                              ),
+                            ]),
+                            TableRow(children: [
+                              Column(
+                                children: [Text(" ")],
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                              ),
+                            ]),
+                            TableRow(children: [
+                              Column(
+                                children: [
+                                  Text("Weakness",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold))
+                                ],
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                              ),
+                            ]),
+                            TableRow(children: [
+                              Column(
+                                children: [
+                                  Text("[ " +
+                                      widget.pokemon.weakness.join(", ") +
+                                      " ]")
+                                ],
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                              ),
+                            ]),
+                            TableRow(children: [
+                              Column(
+                                children: [Text(" ")],
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                              ),
+                            ]),
+                            TableRow(children: [
+                              Column(
+                                children: [
+                                  Text("Previous Evolution",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold))
+                                ],
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                              ),
+                            ]),
+                            TableRow(children: [
+                              Column(
+                                children: [
+                                  Text("[ " +
+                                      widget.pokemon.prevEvolution.join(", ") +
+                                      " ]")
+                                ],
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                              ),
+                            ]),
+                            TableRow(children: [
+                              Column(
+                                children: [Text(" ")],
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                              ),
+                            ]),
+                            TableRow(children: [
+                              Column(
+                                children: [
+                                  Text("Next Evolution",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold))
+                                ],
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                              ),
+                            ]),
+                            TableRow(children: [
+                              Column(
+                                children: [
+                                  Text("[ " +
+                                      widget.pokemon.nextEvolution.join(", ") +
+                                      " ]")
+                                ],
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                              ),
+                            ]),
+                          ],
                         ),
-                      ])),
-                ],
+                      ),
+                    ])),
+                  ],
+                ),
               ),
-            ),
-          ],
-        )
-      ]));
+            ],
+          )
+        ]));
   }
 }
 
@@ -159,7 +197,9 @@ class FavoriteButton extends StatefulWidget {
   final bool isFavorite;
   final VoidCallback onPressed;
 
-  const FavoriteButton({Key? key, required this.isFavorite, required this.onPressed}) : super(key: key);
+  const FavoriteButton(
+      {Key? key, required this.isFavorite, required this.onPressed})
+      : super(key: key);
 
   @override
   _FavoriteButtonState createState() => _FavoriteButtonState();
@@ -177,13 +217,14 @@ class _FavoriteButtonState extends State<FavoriteButton> {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-        icon: icon,
-        onPressed: () {
-      widget.onPressed();
-      setState(() {
-        icon = Icon(widget.isFavorite ? Icons.favorite_border : Icons.favorite);
-      });
-        },
+      icon: icon,
+      onPressed: () {
+        widget.onPressed();
+        setState(() {
+          icon =
+              Icon(widget.isFavorite ? Icons.favorite_border : Icons.favorite);
+        });
+      },
     );
   }
 }
